2090bd96affe254c6d4a43a9208c619762ed61ee
yeonjuko <yjoogo@gmail.com>
2023-04-28 04:24:39 +0900

    service test

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,7 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
-        // id Long타입이라서 그냥 1이면 컴파일 오류남
+        // id Long타입이라서 그냥 1이면 컴파일 오류 발생
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
