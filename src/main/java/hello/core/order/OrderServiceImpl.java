575ea90a033e1a046a1efe5e060cfd57ca5151eb
yeonjuko <yjoogo@gmail.com>
2023-04-28 14:34:05 +0900

    주문 도메인

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 주문 생성 요청(createOrder)이 들어오면 멤버를 조회하고
+        Member member = memberRepository.findById(memberId);
+        // 할인 정책에 회원을 넘겨서 할인이 적용되는지 확인. grade만 넘길지 member 자체를 넘길지는 선택(상황에 따라)
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
